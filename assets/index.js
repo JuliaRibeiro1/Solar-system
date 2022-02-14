6e22bd7a774b1ca2a35dd5241038df01a12e916e
Julia <hijulia259@gmail.com>
2022-02-14 13:37:43 -0300

    falta conteudo

--- assets/index.js
+++ assets/index.js
@@ -10,14 +10,30 @@ function moveTitle() {
 
 }
 function moveText() {
-    if(window.scrollY >300 && window.scrollY < 800){
+    if(window.scrollY >300 && window.scrollY < 1000){
         document.querySelector('.introductionText').style.cssText = "transform:translateX(0%);transition:ease-in-out 1s;"
     }
     else {
         document.querySelector('.introductionText').style.cssText = "transform:translateX(15%);transition:ease-in-out 1s"
     
 }
+}
+var transitionPhrase = "THE EIGHT PLANETS"
+var letters = transitionPhrase.split('')
+function transitionText() {
+    if(window.scrollY > 910 && window.scrollY < 1400) {
+        if(letters.length > 0){
+            document.querySelector('.transitionText').innerHTML += letters.shift()
+        }
+
+}else {
+    
 
 }
+}
+
+
+setInterval(transitionText,5000)
+window.addEventListener('scroll',transitionText)
 window.addEventListener('scroll',moveTitle)
 window.addEventListener('scroll',moveText)
